2709ff084c58b1cf7435ebaa112622b0ee3546d6
peterabel25 <peterabel2504@gmail.com>
2022-07-25 14:47:44 +0300

    changed the content of the listtiles

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
+
+
 
 import 'package:flutter/material.dart';
 
@@ -17,53 +19,84 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
           appBar: AppBar(
             backgroundColor: Color.fromARGB(255, 241, 98, 146),
-            title:  Text("demo application"),
+            title:  Text("demo application"), 
           ),
           body: ListView(
             children: [
               ListTile(
-                leading: CircleAvatar(child: Icon(Icons.account_circle)),
-                title: Text("Nanna"),
-                
-                subtitle: Row(children: [
-                  Icon(Icons.done_all),
-                  Text("kesho hackathon")
-                ],),
-                trailing: Text("20:00"),
-              ),
-              ListTile(
-                leading: CircleAvatar(child: Icon(Icons.account_circle)),
-                title: Text("james"),
-                subtitle: Row(children: [
-                  Icon(Icons.done_all),
-                  Text("kesho hackathon")
-                ],),
-                trailing: Text("20:00"),
-              ),
-              ListTile(
-                leading: CircleAvatar(child: Icon(Icons.account_circle)),
+                leading:CircleAvatar(backgroundImage:AssetImage("assets/pic1.jpg")) ,
                 title: Row(
                   children: [
-                    Text("denis"),
+                    Text("AJIRACHAP-24(Nafasi za Kazi)"),
                     Spacer(),
-                    Text("Yesterday"),
+                    Text("12:23")
                   ],
                 ),
-                subtitle: Row(children: [
-                  Icon(Icons.done_all),
-                  Text("kesho hackathon")
-                ],),
-                trailing: Text("20:00"),
+                subtitle: Row(
+                  children: [
+                    Text("+255 625 554 836 : TANGAZO " ),
+                    Spacer(),
+                    
+                    CircleAvatar(radius:15, child: Text("618"),backgroundColor:Color.fromARGB(255, 42, 90, 43) ,)
+                  ],
+                ),
+              ),
+               ListTile(
+                leading:CircleAvatar(
+                  backgroundImage: AssetImage("assets/pic2.jpg"),
+                ) ,
+                title: Row(
+                  children: [
+                    Text("Mwarabu"),
+                    Spacer(),
+                    Text("Yesterday")
+                  ],
+                ),
+                subtitle: Row(
+                  children: [
+                    Text("Alhamdulliah cjambo cjui we ? " ),
+                    Spacer(),
+                    CircleAvatar(radius:10, child: Text("1"),backgroundColor:Color.fromARGB(255, 42, 90, 43) ,)
+                  ],
+                ),
               ),
               ListTile(
-                leading: CircleAvatar(child: Icon(Icons.account_circle)),
-                title: Text("hossam"),
-                subtitle: Row(children: [
-                  Icon(Icons.done_all),
-                  Text("kesho hackathon")
-                ],),
-                trailing: Text("20:00"),
+                leading:CircleAvatar(backgroundImage:AssetImage("assets/pic3.jpg"),) ,
+                title: Row(
+                  children: [
+                    Text("Pizzle Tg"),
+                    Spacer(),
+                    Text("19/07/2022")
+                  ],
+                ),
+                subtitle: Row(
+                  children: [
+                    Icon(Icons.done_all),
+                    SizedBox(width:5 ,),
+                    Text("maskini na wanawe" ),
+                  ],
+                ),
+              ),
+              ListTile(
+                leading:CircleAvatar(backgroundImage: AssetImage("assets/pic3.jpg"),) ,
+                title: Row(
+                  children: [
+                    Text("+255 745 219 910"),
+                    Spacer(),
+                    Text("19/07/2022")
+                  ],
+                ),
+                subtitle: Row(
+                  children: [
+                    Text("Hi,mambo Naomba nikusevu B..... " ),
+                    Spacer(),
+                    CircleAvatar(radius:10, child: Text("1"),backgroundColor:Color.fromARGB(255, 42, 90, 43) ,)
+                  ],
+                ),
               )
+              
+
+
             ],
           )),
     );
